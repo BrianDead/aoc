1b4fa62773ac05c473a455296edf481c989e0696
BrianDead <rich@baldrys.ca>
2022-12-19 00:41:06 -0800

    Removed progress printing - 25% speed increase?

--- 2022/d16/d16b.pl
+++ 2022/d16/d16b.pl
@@ -18,7 +18,8 @@ print Dumper \%v;
 print Dumper \%vs;
 
 my $timelimit=26;
-my $answer=checkpath("AA", "AA", 0, "|AA-AA|", %vs);
+#my $answer=checkpath("AA", "AA", 0, "|AA-AA|", %vs);
+my $answer=checkpath("AA", "AA", 0,  %vs);
 
 printf("Answer is %d\n", $answer);
 
@@ -28,7 +29,7 @@ sub checkpath {
 	my $hnode=shift;
 	my $enode=shift;
 	my $ts=shift;
-	my $path=shift;
+#	my $path=shift;
 	my %vs=@_;
 
 	return 0 if($ts>=$timelimit);
@@ -39,10 +40,10 @@ sub checkpath {
 			$key=$key.$v;
 		}
 	}
-	printf("memo{%s} is %s\n", $key, $memo{$key});
+#	printf("memo{%s} is %s\n", $key, $memo{$key});
 	return $memo{$key} if(defined($memo{$key}));
 
-	printf("Checking h=%s (%d), e=%s (%d) at time %d from %s\n", $hnode, $vs{$hnode}, $enode, $vs{$enode}, $ts, $path);
+#	printf("Checking h=%s (%d), e=%s (%d) at time %d from %s\n", $hnode, $vs{$hnode}, $enode, $vs{$enode}, $ts, $path);
 
 	my $bestpath=0;
 
@@ -50,7 +51,8 @@ sub checkpath {
 		$vs{$hnode}=0;
 		my $thisflow=$v{$hnode}{"rate"}*($timelimit-($ts+1));
 		foreach my $en (@{$v{$enode}{"cons"}}) {
-			$bestpath=max($bestpath, $thisflow+checkpath($hnode, $en, $ts+1, $path."|".$hnode."!-".$en, %vs));
+#			$bestpath=max($bestpath, $thisflow+checkpath($hnode, $en, $ts+1, $path."|".$hnode."!-".$en, %vs));
+			$bestpath=max($bestpath, $thisflow+checkpath($hnode, $en, $ts+1, %vs));
 		}
 		$vs{$hnode}=$v{$hnode}{"rate"};
 	}
@@ -58,13 +60,15 @@ sub checkpath {
 		$vs{$enode}=0;
 		my $thisflow=$v{$enode}{"rate"}*($timelimit-($ts+1));
 		foreach my $hn (@{$v{$hnode}{"cons"}}) {
-			$bestpath=max($bestpath, $thisflow+checkpath($hn, $enode, $ts+1, $path."|".$hn."-".$enode."!", %vs));
+#			$bestpath=max($bestpath, $thisflow+checkpath($hn, $enode, $ts+1, $path."|".$hn."-".$enode."!", %vs));
+			$bestpath=max($bestpath, $thisflow+checkpath($hn, $enode, $ts+1, %vs));
 		}
 		$vs{$enode}=$v{$enode}{"rate"};
 	}
 	if($vs{$enode} && $vs{$hnode} && !($enode eq $hnode)) {
 		$vs{$enode}=0; $vs{$hnode}=0;
-		$bestpath=max($bestpath, ($v{$hnode}{"rate"}+$v{$enode}{"rate"})*($timelimit-($ts+1))+checkpath($hnode, $enode,$ts+1, $path."|".$hnode."!-".$enode."!",%vs));
+#		$bestpath=max($bestpath, ($v{$hnode}{"rate"}+$v{$enode}{"rate"})*($timelimit-($ts+1))+checkpath($hnode, $enode,$ts+1, $path."|".$hnode."!-".$enode."!",%vs));
+		$bestpath=max($bestpath, ($v{$hnode}{"rate"}+$v{$enode}{"rate"})*($timelimit-($ts+1))+checkpath($hnode, $enode,$ts+1, %vs));
 		$vs{$enode}=$v{$enode}{"rate"};
 		$vs{$hnode}=$v{$hnode}{"rate"};
 	}
@@ -72,11 +76,12 @@ sub checkpath {
  	# Now check paths without turning on this valve
 	foreach my $nn (@{$v{$hnode}{"cons"}}) {
 		foreach my $en (@{$v{$enode}{"cons"}}) {
-			$bestpath=max(checkpath($nn, $en, $ts+1,$path."|".$nn."-".$en,%vs),$bestpath);
+#			$bestpath=max(checkpath($nn, $en, $ts+1,$path."|".$nn."-".$en,%vs),$bestpath);
+			$bestpath=max(checkpath($nn, $en, $ts+1,%vs),$bestpath);
 		}
 	}
 
-	printf("Done checking h=%s (%d), e=%s (%d) at time %d - bestpath %d from %s\n", $hnode, $vs{$hnode}, $enode, $vs{$enode}, $ts, $bestpath, $path);
+#	printf("Done checking h=%s (%d), e=%s (%d) at time %d - bestpath %d from %s\n", $hnode, $vs{$hnode}, $enode, $vs{$enode}, $ts, $bestpath, $path);
 #	printf("Memoize %s as %d\n", $key, $bestpath);
 	$memo{$key}=$bestpath;
 

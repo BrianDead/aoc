99a59a8e2b7fb3ebf10a997293324c05f504b062
Rich Baldry <rich@baldrys.ca>
2022-12-31 15:32:28 -0800

    Fixed hash for full length (duh)

--- 2022/d16/d16b.c
+++ 2022/d16/d16b.c
@@ -13,7 +13,7 @@ typedef struct node_s {
     int nc;
     char conns[MAX_CONNS][2+1];
 } node;
-
+  
 static node nodes[MAX_NODES];
 static int nn[26*26];
 static int nc=0;
@@ -27,7 +27,7 @@ static inline int get_vi(int *vs, char *valve) {
 
 static inline void set_vi(int *vs, char *valve, int s) {
     vs[((valve[0]-'A')*26)+(valve[1]-'A')]=s;
-    printf("Set Valve %s - %d (%d)\n", valve, s, vs[((valve[0]-'A')*26)+(valve[1]-'A')]);
+    printf("Set Valve %s [%d] - %d (%d)\n", valve, ((valve[0]-'A')*26)+(valve[1]-'A'), s, vs[((valve[0]-'A')*26)+(valve[1]-'A')]);
 }
 
 int read(int *vs) {
@@ -85,14 +85,29 @@ void printnodes(int nc) {
 #define TIMELIMIT 26
 
 
-static inline long int get_hash(int ts, int *svs, char *hnode,char *enode) {
-    long int hash=0l;
+static inline unsigned long int get_phash(int ts, char *hnode, char *enode) {
+    unsigned long int hash=0l;
     int hn=get_vi(nn, hnode);
     int en=get_vi(nn, enode);
     int gn=hn>en?hn:en;
     int ln=hn>en?en:hn;
 
-    hash=((ts*MAX_NODES+gn)*MAX_NODES+ln);
+    return ((ts*MAX_NODES+gn)*MAX_NODES+ln);
+}
+
+static inline unsigned long int get_vhash(int *svs) {
+    unsigned long int hash=0l;
+    
+    for(int i=0; i<nc; i++) {
+        hash=hash|(unsigned long int)((svs[i]?1:0)*pow(2,(i)));
+    }
+
+    return hash;
+}
+
+
+static inline unsigned long int get_ovhash(int *svs) {
+    unsigned long int hash=0l;
     
     unsigned char hb[MAX_NODES/8+1];
     for(int i=0; i<MAX_NODES/8+1; i++) {
@@ -100,32 +115,34 @@ static inline long int get_hash(int ts, int *svs, char *hnode,char *enode) {
     }
 
     for(int i=0; i<nc; i++) {
+        printf("%d - hb[%d] - %d - %02x - %02x",i, (int)i/8, svs[i], (unsigned char)((svs[i]?1:0)*pow(2,(i%8))), hb[i/8]);
         hb[i/8]=hb[i/8]|((unsigned char)((svs[i]?1:0)*pow(2,(i%8))));
+        printf("%02x\n",hb[i/8]);
     }
 
-    hash=((((hash*256+hb[3])*256+hb[2])*256+hb[1])*256+hb[0]);
-
-//    printf("Hash %ld: ts=%d %02x%02x%02x%02x h=%s e=%s g=%d l=%d\n",
-//        hash, ts, hb[3],hb[2],hb[1],hb[0],hnode, enode, gn, ln);
+    hash=((((((hb[7]*256+hb[6]*256+hb[5])*256+hb[4])*256+hb[3])*256+hb[2])*256+hb[1])*256+hb[0]);
+ 
+    printf("Hash %16lx: %02x%02x%02x%02x%02x%02x%02x%02x\n",
+        hash, hb[7],hb[6],hb[5],hb[4],hb[3],hb[2],hb[1],hb[0]);
     return hash;
 }
 
 typedef struct centry_s {
-    long int hash;
+    unsigned long int hash;
     int score;
 } centry;
 
-#define MAX_CACHE 100000
+#define MAX_CACHE 10000
 
-static int ce_max=0;
-static centry cache[MAX_CACHE];
+static int ce_max[TIMELIMIT*MAX_NODES*MAX_NODES];
+static centry cache[TIMELIMIT*MAX_NODES*MAX_NODES][MAX_CACHE];
 
-static inline int cache_g(long int hash) {
+static inline int cache_g(unsigned long int phash, unsigned long int vhash) {
     int ret=-1;
 
-    for(int i=0; i<ce_max; i++) {
-        if(cache[i].hash==hash) {
-            ret=cache[i].score;
+    for(int i=0; i<ce_max[phash]; i++) {
+        if(cache[phash][i].hash==vhash) {
+            ret=cache[phash][i].score;
             break;
         }
     }
@@ -134,21 +151,21 @@ static inline int cache_g(long int hash) {
 
 
 int cache_get(int ts,int *svs, char *hnode, char *enode) {
-    return cache_g(get_hash(ts,svs,hnode,enode));
+    return cache_g(get_phash(ts, hnode, enode),get_vhash(svs));
 }
 
-static inline void cache_s(long int hash, int score) {
-    if(ce_max<MAX_CACHE-1) {
-        cache[ce_max].hash=hash;
-        cache[ce_max].score=score;
-        ce_max++;
+static inline void cache_s(unsigned long int phash, unsigned long int vhash, int score) {
+    if(ce_max[phash]<MAX_CACHE-1) {
+        cache[phash][ce_max[phash]].hash=vhash;
+        cache[phash][ce_max[phash]].score=score;
+        ce_max[phash]++;
     } else {
         printf("No cache room");
     }
 }
 
 void cache_set(int ts,int *svs,char *hnode, char *enode, int score) {
-    cache_s(get_hash(ts,svs,hnode,enode), score);
+    cache_s(get_phash(ts, hnode, enode), get_vhash(svs), score);
 }
 
 int countoff(int *svs) {
@@ -170,11 +187,13 @@ int checkpath(char *hnode, char *enode, int ts, int *svs) {
 
     int hn=get_vi(nn, hnode);
     int en=get_vi(nn, enode);
-    long int hash=get_hash(ts, svs, hnode, enode);
+    unsigned long int phash=get_phash(ts, hnode, enode);
+    unsigned long int vhash=get_vhash(svs);
 
-//    printf("Time %d: Human %s Elephant %s Off:%d\n",ts,hnode,enode, countoff(svs));
+    printf("%016lx-%016lx Time %d: Human %s Elephant %s Off:%d\n",phash,vhash,ts,hnode,enode, countoff(svs));
 
-    bestpath=cache_g(hash);
+    bestpath=cache_g(phash, vhash);
+    printf("%016lx-%016lx Seen before %d\n", phash, vhash, bestpath);
     if(bestpath>=0) return bestpath;
     bestpath=0;
 
@@ -203,15 +222,19 @@ int checkpath(char *hnode, char *enode, int ts, int *svs) {
     if(vs[hn]) {
         vs[hn]=0;
         int thisflow=nodes[hn].rate*(TIMELIMIT-(ts+1));
-//        printf("HRate %d for %d at ts=%d (%d) yields %d\n", nodes[hn].rate, hn, ts, TIMELIMIT-(ts+1), thisflow);
+//        printf("HRate %d for %d at ts=%d (%d) yields %d\n",  nodes[hn].rate, hn, ts, TIMELIMIT-(ts+1), thisflow);
         if(cv>1) {
             for(int iv=0; iv<nodes[en].nc; iv++) {
                 int tp;
                 tp=thisflow+checkpath(hnode, nodes[en].conns[iv], ts+1, vs);
-                bestpath=(tp>bestpath) ? tp : bestpath;
+                if(tp>bestpath) {
+                    bestpath=tp;
+                }
             }
         } else {
-            bestpath=(thisflow>bestpath) ? thisflow : bestpath;
+            if(thisflow>bestpath) {
+                bestpath=thisflow;
+            }
         }
 
         vs[hn]=nodes[hn].rate;
@@ -220,15 +243,19 @@ int checkpath(char *hnode, char *enode, int ts, int *svs) {
     if(vs[en]) {
         vs[en]=0; 
         int thisflow=nodes[en].rate*(TIMELIMIT-(ts+1));
-//        printf("ERate %d for %d at ts=%d (%d) yields %d\n", nodes[en].rate, en, ts, TIMELIMIT-(ts+1), thisflow);
+//        printf("ERate %d for %d at ts=%d (%d) yields %d\n",  nodes[en].rate, en, ts, TIMELIMIT-(ts+1), thisflow);
         if(cv>1) {
             for(int iv=0; iv<nodes[hn].nc; iv++) {
                 int tp;
                 tp=thisflow+checkpath(nodes[hn].conns[iv], enode, ts+1, vs);
-                bestpath=(tp>bestpath) ? tp : bestpath;
+                if(tp>bestpath) {
+                    bestpath=tp;
+                }
             }
         } else {
-            bestpath=(thisflow>bestpath) ? thisflow : bestpath;
+            if(thisflow>bestpath) {
+                bestpath=thisflow;
+            }
         }
         vs[en]=nodes[en].rate;
     }
@@ -236,11 +263,13 @@ int checkpath(char *hnode, char *enode, int ts, int *svs) {
     for(int ihv=0; ihv<nodes[hn].nc;ihv++) {
         for(int iev=0; iev<nodes[en].nc; iev++) {
             int tp=checkpath(nodes[hn].conns[ihv],nodes[en].conns[iev],ts+1,vs);
-            bestpath=(tp>bestpath) ? tp : bestpath;
+            if(tp>bestpath) {
+                bestpath=tp;
+            }
         }
     }
-//    printf("Time %d: Best %d - %d (%d to go) yields %d\n", ts, hn, en, TIMELIMIT-(ts+1), bestpath);
-    cache_s(hash, bestpath);
+    printf("%16lx-%16lx Time %d: Best %d - %d (%d to go) yields %d\n", phash, vhash, ts, hn, en, TIMELIMIT-(ts+1), bestpath);
+    cache_s(phash, vhash, bestpath);
     return bestpath;
 
 }
@@ -248,13 +277,18 @@ int checkpath(char *hnode, char *enode, int ts, int *svs) {
 int main(int argc, char * argv) {
     int vs[MAX_NODES];
 
-    printf("long=%ld\n", sizeof(long));
+    printf("long=%ld double=%ld\n", sizeof(long), sizeof(double));
+
+    for(int i=0; i<TIMELIMIT; i++) {
+        ce_max[i]=0;
+    }
 
     for(int i=0; i<MAX_NODES; i++) {
         vs[i]=0;
     }
 
     nc=read(vs);
+    printf("nc=%d\n", nc);
     printnodes(nc);
     for(int i=0; i<MAX_NODES; i++) {
         if(vs[i]) {
@@ -263,5 +297,4 @@ int main(int argc, char * argv) {
     }
 
     printf("Answer is %d\n", checkpath("AA", "AA", 0, vs));
-
 }

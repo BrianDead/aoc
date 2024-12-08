28f7d4de1abbfaea2f6776939e49911f7957ce30
Rich Baldry <rich@baldrys.ca>
2024-12-08 15:07:35 -0800

    Done part 2 of day 7 although not 100% sure how

--- 2024/d07/d07a.pl
+++ 2024/d07/d07a.pl
@@ -3,7 +3,6 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use bignum;
 
 my @list=map {
 	chomp;
@@ -24,20 +23,9 @@ for my $try (@list) {
 		print("Considering $op - ");
 		my @npa=map {
 			my @rv=();
-			print("from $_ ");
-			my $nm=$_ * $op;
-			my $na=$_ + $op;
-			if($nm > $try->[0]) {
-				if($na <= $try->[0]) {
-					@rv=($na);
-				}
-			} else {
-				if($na > $try->[0]) {
-					@rv=($nm);
-				} else {
-					@rv=($na, $nm);
-				}
-			
+			if($_<=$try->[0]) {
+				print("from $_ ");
+				@rv=($_ * $op, $_ + $op);
 			}
 			@rv
 		} @pa;
@@ -50,4 +38,45 @@ for my $try (@list) {
 	print("\n");
 }
 
-print("Answer: $answer1\n")
+print("Answer: $answer1\n");
+
+sub calculate {
+	my $tgt=shift;
+	my $rt=shift;
+	my $to=shift;
+	my @opd=@_;
+
+#	print("Calculating target $tgt, rt $rt, to $to\n");
+#	print Dumper \@opd;
+
+	if(scalar @opd) {
+		return (
+			calculate($tgt, ($rt<0 ? 0 : $rt) + $to, @opd ),
+			calculate($tgt, ($rt<0 ? 1 : $rt) * $to, @opd),
+			calculate($tgt, ($rt<0 ? '' : $rt).$to+0, @opd)
+			);
+	} else {
+		return ( 
+			$rt + $to,
+			$rt * $to,
+			"$rt$to"+0
+		)
+	}
+
+}
+
+my $answer2=0;
+
+for my $try (@list) {
+	my @opds=@{$try->[1]};
+	my $start=shift(@opds);
+	my @results=calculate($try->[0], $start, @opds);
+
+#	print Dumper \@results;
+
+	print("--- Success!") if(grep($_==$try->[0] , @results));
+
+	$answer2+=$try->[0] if(grep($_==$try->[0], @results));
+	print("\n");
+}
+print("Answer: $answer2\n");

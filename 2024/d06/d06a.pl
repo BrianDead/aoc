df7749678c92f1e23e7b90a6fb244dfad8f8dc02
Rich Baldry <rich@baldrys.ca>
2024-12-08 21:02:50 -0800

    Tidied up and solve both parts at the same time. Still quite slow.

--- 2024/d06/d06a.pl
+++ 2024/d06/d06a.pl
@@ -27,33 +27,6 @@ for my $i (0..$h-1) {
 #	print("\n");
 }
 
-#print("Start: $px, $py facing $pd\n");
-
-my $sx=$px;
-my $sy=$py;
-my $sd=$pd;
-
-my %visited;
-
-while($px>=0 && $py>=0 && $px<$w && $py<$h) {
-	my $tx=$px+$dir{$pd}->[0];
-	my $ty=$py+$dir{$pd}->[1];
-#	print("At $px, $py heading $pd - next $tx, $ty ");
-	$visited{"$px,$py"}++;
-	if($tx>=0 && $ty>=0 && $tx<$w && $ty<$h && $m[$ty][$tx] eq '#') {
-#		print("which is a # so turning");
-
-		$pd=$dir{$pd}->[2];
-		$tx=$px+$dir{$pd}->[0];
-		$ty=$py+$dir{$pd}->[1];
-#		print("now heading $pd - next $tx, $ty ");
-	}
-	$px=$tx; $py=$ty;
-#	print("\n");
-}
-
-printf("Answer 1: %d\n", scalar keys %visited);
-
 sub testpath {
 	my $px=shift;
 	my $py=shift;
@@ -64,15 +37,11 @@ sub testpath {
 
 	my $loop=0;
 
-#	print("LOOP DETECTOR ENABLED\n");
-
 	while($px>=0 && $py>=0 && $px<$w && $py<$h && !$loop) {
 		my $tx=$px+$dir{$pd}->[0];
 		my $ty=$py+$dir{$pd}->[1];
 
-#		print("	At $px, $py heading $pd - next $tx, $ty ");
 		if(defined($ld{"$px,$py,$pd"}) ) {
-#			print("Loop!\n");
 			$loop=1;
 			last;
 		}
@@ -82,14 +51,10 @@ sub testpath {
 			($m[$ty][$tx] eq '#' || ($tx==$ox && $ty==$oy)) ) {
 			#there's already an obstruction
 			$pd=$dir{$pd}->[2];
-#			$tx=$px+$dir{$pd}->[0];
-#			$ty=$py+$dir{$pd}->[1];
 			$tx=$px;
 			$ty=$py;
-#			print("now heading $pd - next $tx, $ty ");
 		}
 		$px=$tx; $py=$ty;
-#		print("\n");
 	}
 	return $loop;
 }
@@ -97,10 +62,7 @@ sub testpath {
 my %visits=();
 my %pcons=();
 my $answer3=0;
-%visited=();
-$px=$sx;
-$py=$sy;
-$pd=$sd;
+my %visited=();
 
 while($px>=0 && $py>=0 && $px<$w && $py<$h) {
 
@@ -109,37 +71,24 @@ while($px>=0 && $py>=0 && $px<$w && $py<$h) {
 
 	my $tx=$px+$dir{$pd}->[0];
 	my $ty=$py+$dir{$pd}->[1];
-#	print("At $px, $py heading $pd - next $tx, $ty ");
 
 	if($tx>=0 && $ty>=0 && $tx<$w && $ty<$h) {
 		if($m[$ty][$tx] eq '#') {
-			#there's already an obstruction
+			#there's already an obstruction. Turn and reassess.
 			$pd=$dir{$pd}->[2];
-#			$tx=$px+$dir{$pd}->[0];
-#			$ty=$py+$dir{$pd}->[1];
 			$tx=$px;
 			$ty=$py;
-#			print("now heading $pd - next $tx, $ty ");
 		} elsif(! defined($visited{"$tx,$ty"}) ) {
-#		 ! defined($visits{"$tx,$ty,<"})
-#				&& ! defined($visits{"$tx,$ty,>"})
-#				&& ! defined($visits{"$tx,$ty,^"})
-#				&& ! defined($visits{"$tx,$ty,v"}) ) {
 			# no obstruction, but consider if putting one here would create a loop
 			# unless we've already passed through this location, in which case, 
 			# there can't be an obstruction here.
 			my $id=$dir{$pd}->[2];
-#			my $ix=$px+$dir{$id}->[0];
-#			my $iy=$py+$dir{$id}->[1];
-#			print("testing $px,$py,$id ");
 			if(testpath($px,$py,$id,$tx,$ty,%visits)) {
 				$pcons{"$tx,$ty"}++;
-#				print("Loop!\n");
 			}
 		}
 	}
 	$px=$tx; $py=$ty;
-#	print("\n");
 }
 
 printf("Answer 1: %d\n", scalar keys %visited);

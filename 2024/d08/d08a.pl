d23db558faad7cb828c86c6b21db1ae11cc28f40
Rich Baldry <rich@baldrys.ca>
2024-12-09 19:27:05 -0800

    Done part 2 of day 8

--- 2024/d08/d08a.pl
+++ 2024/d08/d08a.pl
@@ -59,3 +59,49 @@ for my $xm (keys %xmers) {
 printf("Answer: %d\n", scalar keys %anodes);
 
 #print Dumper \%anodes;
+
+my %anodes2=();
+
+for my $xm (keys %xmers) {
+	my @tt=@{$xmers{$xm}};
+	while((scalar @tt ) > 1) {
+		my $n1=shift(@tt);
+		for my $n2 (@tt) {
+			my $dx=$n2->[0]-$n1->[0];
+			my $dy=$n2->[1]-$n1->[1];
+
+			my $p1x=$n2->[0];
+			my $p1y=$n2->[1];
+
+			while ($p1x>=0 && $p1y>=0 && $p1x<$w && $p1y<$h) {
+				$anodes2{"$p1x,$p1y"}++;
+				$p1x+=$dx;
+				$p1y+=$dy;
+			}
+
+
+			my $p2x=$n1->[0];
+			my $p2y=$n1->[1];
+
+			while ($p2x>=0 && $p2y>=0 && $p2x<$w && $p2y<$h) {
+				$anodes2{"$p2x,$p2y"}++;
+				$p2x-=$dx;
+				$p2y-=$dy;
+			}
+
+		}
+	}
+}
+
+for my $i (0..$h-1) {
+	for my $j (0..$w-1) {
+		if($anodes2{"$j,$i"}) {
+			print '#';
+		} else {
+			print '.';
+		}
+	}
+	print "\n";
+}
+
+printf("Answer2: %d\n", scalar keys %anodes2);
